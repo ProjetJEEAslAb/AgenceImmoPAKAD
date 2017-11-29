feae5b17ff186f3dfe597d3641ca4eec6966ebe6
inti0243 <inti0243@192.168.1.166>
2017-11-29 17:56:19 +0100

    Kikky --> rechercher et modifier classes

--- 10_ProjetImmobilier/src/main/webapp/assets/js/controllers/ClassesStandardController.js
+++ 10_ProjetImmobilier/src/main/webapp/assets/js/controllers/ClassesStandardController.js
@@ -27,8 +27,114 @@ immoApp.controller("listeClassesStandardCtrl",
 						function(callBack) {
 
 							if (callBack != undefined && callBack != "") {
-
+								console.log(callBack)
 								$location.path("listeClasses");
+							}
+						});
+			}
+
+		})
+
+.controller(
+		"supprClassesStandardCtrl",
+		function($scope, classesStandardProvider, $location) {
+
+			$scope.idClasseStandard = 0;
+
+			$scope.supprimerClasseStandard = function() {
+
+				// Initialise la classe standard à supprimer
+				$scope.classeStandard = {
+					id_cl : $scope.idClasseStandard,
+				}
+
+				// Appel de la fonction du Provider pour supprimer la classe
+				// dans
+				// la bdd
+				classesStandardProvider.deleteClass($scope.classeStandard,
+						function(callBack) {
+
+							$location.path("listeClasses");
+
+						});
+			}
+		})
+
+.controller(
+		"modifClassesStandardCtrl",
+		function($scope, classesStandardProvider, $location) {
+
+			// Initialise la classe standard à modifier
+			$scope.classeStandard = {
+				id_cl : 0,
+				typeBien : "",
+				modeOffre : "",
+				prixMax : 0,
+				superficieMin : 0,
+			}
+
+			// Développement de la méthode ajouter() du bouton
+			$scope.modifierClasseStandard = function() {
+
+				// Appel de la fonction du Provider pour modifier la classe dans
+				// la bdd
+				classesStandardProvider.updateClass($scope.classeStandard,
+						function(callBack) {
+
+							if (callBack != undefined && callBack != "") {
+								console.log(callBack)
+								$location.path("listeClasses");
+							}
+						});
+			}
+
+		})
+
+.controller(
+		"rechClassesStandardCtrl",
+		function($scope, classesStandardProvider) {
+
+			// Initialise la classe standard à modifier
+			$scope.classeStandard = {
+				id_cl : 0,
+				typeBien : "",
+				modeOffre : "",
+				prixMax : 0,
+				superficieMin : 0,
+			}
+
+			$scope.idClasseStandard = 0;
+
+			$scope.indice = false;
+			$scope.msg = "";
+
+			$scope.classeStandardFind = {
+				id_cl : 0,
+				typeBien : "",
+				modeOffre : "",
+				prixMax : 0,
+				superficieMin : 0,
+			}
+
+			// Développement de la méthode ajouter() du bouton
+			$scope.rechercherClasseStandard = function() {
+
+				// Appel de la fonction du Provider pour modifier la classe dans
+				// la bdd
+				classesStandardProvider.findClass($scope.idClasseStandard,
+						function(callBack) {
+
+							if (callBack != undefined && callBack != "") {
+								console.log(callBack)
+
+								$scope.classeStandardFind = callBack;
+								$scope.msg = "";
+								$scope.indice = true;
+
+							} else {
+
+								$scope.msg = "Cette classe n'existe pas.";
+								$scope.indice = false;
 							}
 						});
 			}
