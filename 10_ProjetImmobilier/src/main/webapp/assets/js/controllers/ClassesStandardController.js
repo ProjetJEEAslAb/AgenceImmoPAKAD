a6eaf25fd8289e39711340224a1d9e7fba04af97
inti0243 <inti0243@192.168.1.166>
2017-11-29 14:06:25 +0100

    Kikky --> angularjs ajout

--- 10_ProjetImmobilier/src/main/webapp/assets/js/controllers/ClassesStandardController.js
+++ 10_ProjetImmobilier/src/main/webapp/assets/js/controllers/ClassesStandardController.js
@@ -1,8 +1,36 @@
-immoApp.controller("listeClassesStandardCtrl", function($scope,
-		classesStandardProvider, $location) {
+immoApp.controller("listeClassesStandardCtrl",
+		function($scope, classesStandardProvider, $location) {
 
-	classesStandardProvider.getAllClasses(function(callBack) {
-		$scope.classesStandardListe = callBack;
-	});
+			classesStandardProvider.getAllClasses(function(callBack) {
+				$scope.classesStandardListe = callBack;
+			});
+		})
 
-})
+.controller(
+		"ajoutClassesStandardCtrl",
+		function($scope, classesStandardProvider, $location) {
+
+			// Initialise la classe standard à ajouter
+			$scope.classeStandard = {
+				typeBien : "",
+				modeOffre : "",
+				prixMax : 0,
+				superficieMin : 0,
+			}
+
+			// Développement de la méthode ajouter() du bouton
+			$scope.ajouterClasseStandard = function() {
+
+				// Appel de la fonction du Provider pour ajouter la classe dans
+				// la bdd
+				classesStandardProvider.add($scope.classeStandard, function(
+						callBack) {
+
+					if (callBack != undefined && callBack != "") {
+
+						$location.path("listeClasses");
+					}
+				});
+			}
+
+		})
--- 10_ProjetImmobilier/src/main/webapp/assets/js/providers/ClassesStandardProvider.js
+++ 10_ProjetImmobilier/src/main/webapp/assets/js/providers/ClassesStandardProvider.js
@@ -1,6 +1,7 @@
 immoApp.factory("classesStandardProvider", function($http) {
 	var restUrlWS = "http://localhost:8080/10_ProjetImmobilier";
-	var restUrlListe = "/listeClassesStandard"
+	var restUrlListe = "/allclasses";
+	var restUrlAdd = "/classe";
 
 	// Récupérer la liste des classes standard
 	function findAllClasses(callBack) {
@@ -16,8 +17,27 @@ immoApp.factory("classesStandardProvider", function($http) {
 		})
 	}
 
+	// Ajouter une classe standard
+	function addClasses(classeStandardAjout, callBack) {
+		// Envoyer la requête au service
+		$http({
+			method : 'POST',
+			url : restUrlWS + restUrlAdd,
+			data : angular.toJson(classeStandardAjout),
+			headers : {
+				'content-type' : "application/json"
+			}
+		}).then(function succes(response) {
+			callBack = response.data;
+
+		}, function error(response) {
+			console.log("----- Erreur : " + response.statusText)
+		})
+	}
+
 	// Le retour de factory
 	return {
 		getAllClasses : findAllClasses,
+		addClass : addClasses
 	}
 })
