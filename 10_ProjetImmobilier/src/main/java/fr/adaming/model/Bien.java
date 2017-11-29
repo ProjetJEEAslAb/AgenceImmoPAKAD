a037f6cbe897da08e964645842d72672128810ca
inti0232 <inti0232@192.168.1.169>
2017-11-29 12:18:18 +0100

    Démarrage du projet : c'est parti kiki!!

--- 10_ProjetImmobilier/src/main/java/fr/adaming/model/Bien.java
+++ 10_ProjetImmobilier/src/main/java/fr/adaming/model/Bien.java
@@ -1,5 +1,166 @@
 package fr.adaming.model;
 
-public class Bien {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="biens")
+public class Bien {
+	
+	//Declaration des attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="ref")
+	protected int id_b;
+	
+	protected String statut;
+	
+	protected Date dateSoumission;
+	
+	protected Date dateDisponibilite;
+	
+	protected double revenuCadastral;
+	
+	protected double superficie;
+	
+	//Liaisons UML en Java
+	
+	protected Classe classe;
+	
+	protected Agent agent;
+	
+	protected Contrat contrat;
+
+	//Constructeurs
+	public Bien() {
+		super();
+	}
+
+		
+	public Bien(String statut, Date dateSoumission, Date dateDisponibilite, double revenuCadastral, double superficie) {
+		super();
+		this.statut = statut;
+		this.dateSoumission = dateSoumission;
+		this.dateDisponibilite = dateDisponibilite;
+		this.revenuCadastral = revenuCadastral;
+		this.superficie = superficie;
+	}
+
+
+	public Bien(int ref, String statut, Date dateSoumission, Date dateDisponibilite, double revenuCadastral,
+			double superficie) {
+		super();
+		this.ref = ref;
+		this.statut = statut;
+		this.dateSoumission = dateSoumission;
+		this.dateDisponibilite = dateDisponibilite;
+		this.revenuCadastral = revenuCadastral;
+		this.superficie = superficie;
+	}
+
+
+	//Getters et Setters
+	public int getId_b() {
+		return id_b;
+	}
+
+
+	public void setId_b(int id_b) {
+		this.id_b = id_b;
+	}
+
+
+	public String getStatut() {
+		return statut;
+	}
+
+
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+
+
+	public Date getDateSoumission() {
+		return dateSoumission;
+	}
+
+
+	public void setDateSoumission(Date dateSoumission) {
+		this.dateSoumission = dateSoumission;
+	}
+
+
+	public Date getDateDisponibilite() {
+		return dateDisponibilite;
+	}
+
+
+	public void setDateDisponibilite(Date dateDisponibilite) {
+		this.dateDisponibilite = dateDisponibilite;
+	}
+
+
+	public double getRevenuCadastral() {
+		return revenuCadastral;
+	}
+
+
+	public void setRevenuCadastral(double revenuCadastral) {
+		this.revenuCadastral = revenuCadastral;
+	}
+
+
+	public double getSuperficie() {
+		return superficie;
+	}
+
+
+	public void setSuperficie(double superficie) {
+		this.superficie = superficie;
+	}
+
+
+	public Classe getClasse() {
+		return classe;
+	}
+
+
+	public void setClasse(Classe classe) {
+		this.classe = classe;
+	}
+
+
+	public Agent getAgent() {
+		return agent;
+	}
+
+
+	public void setAgent(Agent agent) {
+		this.agent = agent;
+	}
+
+
+	public Contrat getContrat() {
+		return contrat;
+	}
+
+
+	public void setContrat(Contrat contrat) {
+		this.contrat = contrat;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Bien [ref=" + ref + ", statut=" + statut + ", dateSoumission=" + dateSoumission + ", dateDisponibilite="
+				+ dateDisponibilite + ", revenuCadastral=" + revenuCadastral + ", superficie=" + superficie + "]";
+	}
+	
+	
 }
---
+++ 10_ProjetImmobilier/src/main/java/fr/adaming/model/Classe.java
@@ -0,0 +1,111 @@
+package fr.adaming.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="classes")
+public class Classe {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="code")
+	private int id_cl;
+	
+	private String typeBien;
+	
+	private String modeOffre;
+	
+	private double prixMax;
+	
+	private double superficieMin;
+	
+	//Liaisons UML en Java
+	private List<Bien> listeBiens;
+
+	//Constructeurs
+	public Classe() {
+		super();
+	}
+
+	public Classe(String typeBien, String modeOffre, double prixMax, double superficieMin) {
+		super();
+		this.typeBien = typeBien;
+		this.modeOffre = modeOffre;
+		this.prixMax = prixMax;
+		this.superficieMin = superficieMin;
+	}
+
+	public Classe(int id_cl, String typeBien, String modeOffre, double prixMax, double superficieMin) {
+		super();
+		this.id_cl = id_cl;
+		this.typeBien = typeBien;
+		this.modeOffre = modeOffre;
+		this.prixMax = prixMax;
+		this.superficieMin = superficieMin;
+	}
+
+	public int getId_cl() {
+		return id_cl;
+	}
+
+	public void setId_cl(int id_cl) {
+		this.id_cl = id_cl;
+	}
+
+	public String getTypeBien() {
+		return typeBien;
+	}
+
+	public void setTypeBien(String typeBien) {
+		this.typeBien = typeBien;
+	}
+
+	public String getModeOffre() {
+		return modeOffre;
+	}
+
+	public void setModeOffre(String modeOffre) {
+		this.modeOffre = modeOffre;
+	}
+
+	public double getPrixMax() {
+		return prixMax;
+	}
+
+	public void setPrixMax(double prixMax) {
+		this.prixMax = prixMax;
+	}
+
+	public double getSuperficieMin() {
+		return superficieMin;
+	}
+
+	public void setSuperficieMin(double superficieMin) {
+		this.superficieMin = superficieMin;
+	}
+
+	public List<Bien> getListeBiens() {
+		return listeBiens;
+	}
+
+	public void setListeBiens(List<Bien> listeBiens) {
+		this.listeBiens = listeBiens;
+	}
+
+	@Override
+	public String toString() {
+		return "Classe [id_cl=" + id_cl + ", typeBien=" + typeBien + ", modeOffre=" + modeOffre + ", prixMax=" + prixMax
+				+ ", superficieMin=" + superficieMin + "]";
+	}
+	
+	
+	
+
+}
