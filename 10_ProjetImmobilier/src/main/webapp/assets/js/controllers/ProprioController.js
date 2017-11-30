ab1b27147867a0f9021420dbb5aba88be64e5e84
inti0243 <inti0243@192.168.1.166>
2017-11-30 12:19:04 +0100

    Kikky --> provider + controller ajouter proprio

--- 10_ProjetImmobilier/src/main/webapp/assets/js/controllers/ProprioController.js
+++ 10_ProjetImmobilier/src/main/webapp/assets/js/controllers/ProprioController.js
@@ -3,3 +3,27 @@ immoApp.controller("proprioCtrl", function($scope, proprioProvider, $location) {
 		$scope.proprioListe = callBack;
 	});
 })
+
+.controller("ajoutProprioCtrl", function($scope, proprioProvider, $location) {
+
+	// Initialise la classe standard à ajouter
+	$scope.proprioAjout = {
+		nom : "",
+		numPrive : 0,
+		numTravail : 0
+	}
+
+	// Développement de la méthode ajouter() du bouton
+	$scope.ajouterProprio = function() {
+
+		// Appel de la fonction du Provider pour ajouter la classe dans
+		// la bdd
+		proprioProvider.addProprio($scope.proprioAjout, function(callBack) {
+
+			if (callBack != undefined && callBack != "") {
+				console.log(callBack)
+				$location.path("listeProprio");
+			}
+		});
+	}
+})
--- 10_ProjetImmobilier/src/main/webapp/assets/js/providers/ProprioProvider.js
+++ 10_ProjetImmobilier/src/main/webapp/assets/js/providers/ProprioProvider.js
@@ -1,6 +1,7 @@
 immoApp.factory("proprioProvider", function($http) {
 	var restUrlWS = "http://localhost:8080/10_ProjetImmobilier";
 	var restUrlListe = "/allproprietaires";
+	var restUrlAdd = "/proprietaire";
 
 	// Récupérer la liste des propriétaires
 	function findAllProprio(callBack) {
@@ -8,7 +9,7 @@ immoApp.factory("proprioProvider", function($http) {
 		$http({
 			method : 'GET',
 			url : restUrlWS + restUrlListe
-			
+
 		}).then(function succes(response) {
 			callBack(response.data);
 
@@ -16,9 +17,28 @@ immoApp.factory("proprioProvider", function($http) {
 			console.log("----- Erreur : " + response.statusText)
 		})
 	}
-	
+
+	// Ajouter un propriétaire
+	function addProprioPro(proprioAjout, callBack) {
+		// Envoyer la requête au service
+		$http({
+			method : 'POST',
+			url : restUrlWS + restUrlAdd,
+			data : angular.toJson(proprioAjout),
+			headers : {
+				'content-type' : "application/json"
+			}
+
+		}).then(function succes(response) {
+			callBack(response.data);
+
+		}, function error(response) {
+			console.log("----- Erreur : " + response.statusText)
+		})
+	}
 	// Le retour de factory
 	return {
-		getAllProprio : findAllProprio
+		getAllProprio : findAllProprio,
+		addProprio : addProprioPro
 	}
 })
