76dc6d99609d990c6fea0ffeda7fc74cb0e480ca
inti0243 <inti0243@192.168.1.166>
2017-11-30 12:33:19 +0100

    Kikky --> routing

--- 10_ProjetImmobilier/src/main/webapp/assets/js/routing/routing.js
+++ 10_ProjetImmobilier/src/main/webapp/assets/js/routing/routing.js
@@ -25,41 +25,40 @@ immoApp.config(function($routeProvider) {
 		templateUrl : "views/rechClassesStandard.html",
 		controller : "rechClassesStandardCtrl"
 
-			
 	// ----- Locations -------
 	}).when("/ajoutLocation", {
 		templateUrl : "views/ajoutLocation.html",
 		controller : "ajoutLocationCtrl"
 	})
-	
+
 	// ----- Clients -------
 	.when("/ajoutClient", {
 		templateUrl : "views/ajoutClient.html",
 		controller : "ajoutClientCtrl"
 	// --- Redirection par défault
-	})
-	.when("/suppClient", {
+	}).when("/suppClient", {
 		templateUrl : "views/suppClient.html",
 		controller : "suppClientCtrl"
-			
-	})
-	.when("/modifClient", {
+
+	}).when("/modifClient", {
 		templateUrl : "views/modifClient.html",
 		controller : "modifClientCtrl"
-			
-	})
-	.when("/rechClient", {
+
+	}).when("/rechClient", {
 		templateUrl : "views/rechClient.html",
 		controller : "rechClientCtrl"
-			
+
 	})
-	
+
 	// ----- Proprio -------
 	.when("/afficheProprio", {
 		templateUrl : "views/listeProprio.html",
 		controller : "proprioCtrl"
 			
-			
+	}).when("/ajoutProprio", {
+		templateUrl : "views/ajoutProprio.html",
+		controller : "ajoutProprioCtrl"
+
 	// --- Redirection par défault
 	}).otherwise({
 		redirectTo : "/listeClasses"
