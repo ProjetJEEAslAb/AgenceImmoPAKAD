708ed213fb84e8a936fb6aa59d6683f63fa1002b
inti-0265 <inti-0265@192.168.1.170>
2017-12-02 17:48:39 +0100

    Anissa : index modif

--- 10_ProjetImmobilier/src/main/webapp/assets/js/script_Perso/script.js
+++ 10_ProjetImmobilier/src/main/webapp/assets/js/script_Perso/script.js
@@ -1,9 +1,34 @@
-$(function () {
-    var divs = $("div");
-    divs.not("").hide();
-    $("#idmeuble").change(function () {
-        divs.filter(":visible").hide();
-        $($(this).val()).show();
-        return false;
-    });
+$(document).ready(function() {
+	$("#btnPrint").on("click", function() {
+		var conceptStore = $("#dvContainer").html();
+		var printWindow = window.open('', '_blank', 'height=400,width=800');
+		var is_chrome = Boolean(printWindow.chrome);
+		// ecrire le html dans la nouvelle fenêtre
+		printWindow.document.write('<html><head><title>Liste des visites</title>');
+		// printWindow.document.write('<link rel="stylesheet"
+		// href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+		// absolute = true>');
+
+		printWindow.document.write('</head><body>');
+
+		// Add the stylesheet link and inline styles to the new document:
+		 //printWindow.document.write('<link rel="stylesheet" href="resources/css/styleFich.css">');
+
+		printWindow.document.write(conceptStore);
+		printWindow.document.write('</body></html>');
+
+		if (is_chrome) {
+			setTimeout(function() { // wait until all resources loaded
+				printWindow.document.close();
+				printWindow.focus();
+				printWindow.print();
+				printWindow.close();
+			}, 250);
+		} else {
+			printWindow.document.close();
+			printWindow.focus();
+			printWindow.print();
+			printWindow.close();
+		}
+	});
 });
